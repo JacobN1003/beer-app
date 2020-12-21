05fb35677bab55e0af24dd54e118981a69df49a7
Jacob Norenberg <jacobn@jacob.lan>
2020-12-21 13:11:56 -0800

    content security policy fix v1.3

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ const express = require('express')
 const app = express()
 const port = PORT || 5000
 
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+}
 app.listen(port, () => { console.log(`Server listening on port ${port}..`) });
